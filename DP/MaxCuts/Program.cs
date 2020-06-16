f1e141b06afc2260920b327d378b002a3fe791b1
Vartika06 <magicalvartika06@gmail.com>
2020-06-16 11:26:07 +0530

    DP - Maximum cuts of length a or b or c possible in a rope of length n

---
+++ DP/MaxCuts/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaxCuts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 5;
+            var a = 3;
+            var b = 2;
+            var c = 1;
+            var maxCuts = new MaximumCuts().MaxCuts(n, a, b, c);
+            if ( maxCuts == -1)
+            {
+                Console.WriteLine($"No cuts of length {a} or {b} or {c} possible in a rope of length {n}");
+            }
+            else {
+                Console.WriteLine($"Maximum cuts of length either {a} or {b} or {c}, possible in a rope of length {n} : {maxCuts}");
+            }
+        }
+    }
+}
