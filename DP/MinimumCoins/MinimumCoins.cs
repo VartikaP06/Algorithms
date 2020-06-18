992395c5db7f6fd88d84acf2e346db1228fd8be7
Vartika06 <magicalvartika06@gmail.com>
2020-06-18 11:17:45 +0530

    DP - Minimum coins to make a value

---
+++ DP/MinimumCoins/MinimumCoins.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace MinimumCoins
+{
+    public class MinimumCoins
+    {
+        public int MinCoins(int[] coins, int n, int val)
+        {
+            var dp = InitialiseDp(val);
+
+            for (int i = 1; i <= val; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (coins[j] <= i)
+                    {
+                        var sub_res = dp[i - coins[j]];
+                        if (sub_res != int.MaxValue)
+                        {
+                            dp[i] = Math.Min(dp[i], sub_res + 1);
+                        }
+                    }
+                }
+            }
+
+            return dp[val] != int.MaxValue ? dp[val] : -1;
+        }
+
+        private int[] InitialiseDp(int val)
+        {
+            var dp = new int[val+1];
+            for (int i = 1; i <= val; i++)
+            {
+                dp[i] = int.MaxValue;
+            }
+
+            return dp;
+        }
+    }
+}
---
+++ DP/MinimumCoins/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinimumCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coins = new [] { 26, 10, 6 };
+            var val = 39;
+
+            var minCoins = new MinimumCoins().MinCoins(coins, coins.Length, val);
+            if (minCoins == -1)
+            {
+                Console.WriteLine($"Not possible to get {val} as a combination of given coins");
+            }
+            else {
+                Console.WriteLine($"Minimum Coins to make value {val} : {minCoins}");
+            }
+        }
+    }
+}
